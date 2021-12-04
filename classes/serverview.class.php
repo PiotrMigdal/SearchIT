782db1cdfd33d916085091d1a6e6c3fff1d9fa04
Piotr Migdal <Piotr.Migdal@PARSEQ.COM>
2021-12-04 08:11:15 +0000

    Added get servers handler that compares this to server list and change status of button

--- classes/serverview.class.php
+++ classes/serverview.class.php
@@ -2,8 +2,14 @@
 class ServerView extends Server {
     public function showServerButtons() {
         foreach ($this->getServerList() as $server) {
+            //THIS NEED TO GO TO CONTROLLER AND MAPPED ARRAY ADDING THE $SERVER["STATUS"]
+            if(in_array($server["name"], $_GET["servers"])) {
+                $status = "success";
+            } else {
+                $status = "secondary";
+            }
             echo    '<div class="btn-group m-1">
-                            <button class="btn btn-sm btn-secondary" type="submit" name="' . $server["name"] . '" value="true">
+                            <button class="btn btn-sm btn-' . $status . '" type="submit" name="servers[]" value="' . $server["name"] . '">
                                 ' . $server["name"] . '
                             </button>
                             <button type="button" class="btn btn-sm btn-success" data-toggle="popover" title="Last updated" data-content="' . $server["updated_at"] . '">

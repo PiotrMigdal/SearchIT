950f45f387898389d4e12d1a22211c4cb3cd0526
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2022-05-22 15:41:33 +0100

    Refactored

--- classes/Models/Server.class.php
+++ classes/Models/Server.class.php
@@ -6,20 +6,22 @@ namespace Models;
 // - add that mini
 // - add select all and clear all in JS
 // - change folder names to Live and UAT and that db import them according to this. if someone adds ansible, they will be able just to add folder
-class Server extends Dbh {
+class Server extends Dbh
+{
     public function serverList(): array
     {
         // Get them from DB table servers
         $db = Dbh::getInstance();
-        $stmt = $db->connect()->prepare("SELECT * FROM servers ORDER BY environment ASC, name ASC");
+        $stmt = $db->connect()->prepare('SELECT * FROM servers ORDER BY environment ASC, name ASC');
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
     public function envList(): array
     {
         // Get them from DB table servers
         $db = Dbh::getInstance();
-        $stmt = $db->connect()->prepare("SELECT environment FROM servers GROUP BY environment");
+        $stmt = $db->connect()->prepare('SELECT environment FROM servers GROUP BY environment');
         $stmt->execute();
         return $stmt->fetchAll();
     }
--- classes/Models/Task.class.php
+++ classes/Models/Task.class.php
@@ -4,19 +4,20 @@ namespace Models;
 
 use PDO;
 
-class Task extends Dbh {
+class Task extends Dbh
+{
 
     public function searchTasks($servers, $search, $activeonly): array
     {
-        $activeonly ? $active = "and Active = 'true'" : $active = "";
-        $in  = str_repeat('?,', count($servers) - 1) . '?';
-        $searchLike = "%$search%";
+        $activeonly ? $active = 'and Active = \'true\'' : $active = '';
+        $in = str_repeat('?,', count($servers) - 1) . '?';
+        $searchLike = '%$search%';
 
         $db = Dbh::getInstance();
-        $stmt = $db->connect()->prepare("SELECT * FROM tasks WHERE 
+        $stmt = $db->connect()->prepare('SELECT * FROM tasks WHERE 
                       Server in ($in) $active and 
                       (GroupName like ? or JobName like ? or JobDescription like ? or TaskName like ? or TaskDescription like ? or DestinationDirectory like ? or SourceFolder like ? or IncludeFileMask like ?)
-                      ORDER BY ID");
+                      ORDER BY ID');
 
         $stmt->execute(array_merge($servers, [$searchLike, $searchLike, $searchLike, $searchLike, $searchLike, $searchLike, $searchLike, $searchLike]));
 
